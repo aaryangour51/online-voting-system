3c23bc2ec128fce0adfa3f5910b78e72e4f2e4bb
aaryangour51 <aaryangour51@gmail.com>
2026-05-12 23:31:37 +0530

    Update DBConnection to use TiDB Cloud credentials

--- src/main/java/com/voting/connection/DBConnection.java
+++ src/main/java/com/voting/connection/DBConnection.java
@@ -6,30 +6,48 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
+    // TiDB Cloud URL
     private static final String URL =
-            "jdbc:mysql://yamabiko.proxy.rlwy.net:30832/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        "dhnCS0tDRUNftcD4@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/sys?sslMode=VERIFY_IDENTITY";
 
-    private static final String USER = "root";
+    // TiDB Username
+    private static final String USER =
+        "online-voting";
 
-    private static final String PASSWORD = "Voting@123";
+    // TiDB Password
+    private static final String PASSWORD =
+        "dhnCS0tDRUNftcD4";
 
     static {
+
         try {
+
             Class.forName("com.mysql.cj.jdbc.Driver");
+
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException("MySQL Driver not found", e);
+
+            throw new RuntimeException(
+                    "MySQL Driver not found", e);
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection()
+            throws SQLException {
+
+        return DriverManager.getConnection(
+                URL, USER, PASSWORD);
     }
 
     public static void closeConnection(Connection conn) {
+
         if (conn != null) {
+
             try {
+
                 conn.close();
+
             } catch (SQLException e) {
+
                 e.printStackTrace();
             }
         }
