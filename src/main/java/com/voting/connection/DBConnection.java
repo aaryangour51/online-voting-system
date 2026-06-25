ae76e12514b7a472e596f7314d935ab1569cfeb1
aaryangour51 <aaryangour51@gmail.com>
2026-06-25 15:15:41 +0530

    Change DB connection to local MySQL server
    Updated database connection details to use local MySQL server.

--- src/main/java/com/voting/connection/DBConnection.java
+++ src/main/java/com/voting/connection/DBConnection.java
@@ -1,53 +1,31 @@
 package com.voting.connection;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
 
-    // TiDB Cloud URL
-    private static final String URL =
-        "dhnCS0tDRUNftcD4@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/sys?sslMode=VERIFY_IDENTITY";
-
-    // TiDB Username
-    private static final String USER =
-        "online-voting";
-
-    // TiDB Password
-    private static final String PASSWORD =
-        "dhnCS0tDRUNftcD4";
+    private static final String URL = "jdbc:mysql://localhost:3306/voting_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "787977";  // Change to your MySQL password
 
     static {
-
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-
         } catch (ClassNotFoundException e) {
-
-            throw new RuntimeException(
-                    "MySQL Driver not found", e);
+            throw new RuntimeException("MySQL Driver not found", e);
         }
     }
 
-    public static Connection getConnection()
-            throws SQLException {
-
-        return DriverManager.getConnection(
-                URL, USER, PASSWORD);
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
     public static void closeConnection(Connection conn) {
-
         if (conn != null) {
-
             try {
-
                 conn.close();
-
             } catch (SQLException e) {
-
                 e.printStackTrace();
             }
         }
